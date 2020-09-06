0f21c9695ad0419950d24c848548633caa70a9be
abeidahmed <abeidahmed92@gmail.com>
2020-09-06 21:00:30 +0530

    make images available in vue

--- app/javascript/components/preview.vue
+++ app/javascript/components/preview.vue
@@ -38,7 +38,12 @@
               class="inline-flex items-center justify-center text-gray-500 hover:text-gray-700"
               @click="copy"
             >
-              Copy
+              <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"></path>
+                <path
+                  d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"
+                ></path>
+              </svg>
             </button>
           </div>
         </div>
