ff28dbf93a35c6b1d74ab2f230badb40ef66be7c
abeidahmed <abeidahmed92@gmail.com>
2020-09-07 11:03:49 +0530

    disable turbolinks when navigating to show category page so that js gets loaded on load event

--- app/javascript/components/preview.vue
+++ app/javascript/components/preview.vue
@@ -41,10 +41,10 @@
               @click="copy"
             >
               <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
-                <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"></path>
+                <path d="M8 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z" />
                 <path
                   d="M6 3a2 2 0 00-2 2v11a2 2 0 002 2h8a2 2 0 002-2V5a2 2 0 00-2-2 3 3 0 01-3 3H9a3 3 0 01-3-3z"
-                ></path>
+                />
               </svg>
             </button>
           </div>
@@ -120,7 +120,7 @@ export default {
         this.$refs.iframe.contentWindow.document.body.scrollHeight + 'px';
     },
   },
-  mounted() {
+  created() {
     window.addEventListener('load', () => {
       this.computeHeight;
     });
