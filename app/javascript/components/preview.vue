f327cfc5aa47aa45b6006cca098b1e4dc4274d35
abeidahmed <abeidahmed92@gmail.com>
2020-09-07 14:20:45 +0530

    make code toggleable on small screens

--- app/javascript/components/preview.vue
+++ app/javascript/components/preview.vue
@@ -51,9 +51,17 @@
         </div>
         <div class="sm:hidden">
           <button
-            class="inline-block px-3 py-3 font-medium leading-none text-gray-500 rounded-lg focus:outline-none"
+            class="inline-block px-3 py-3 font-medium leading-none rounded-lg focus:outline-none"
+            @click="showCode = !showCode"
+            :class="showCode ? 'bg-indigo-50 text-indigo-700' : 'text-gray-500'"
           >
-            Code
+            <svg viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+              <path
+                fill-rule="evenodd"
+                d="M12.316 3.051a1 1 0 01.633 1.265l-4 12a1 1 0 11-1.898-.632l4-12a1 1 0 011.265-.633zM5.707 6.293a1 1 0 010 1.414L3.414 10l2.293 2.293a1 1 0 11-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0zm8.586 0a1 1 0 011.414 0l3 3a1 1 0 010 1.414l-3 3a1 1 0 11-1.414-1.414L16.586 10l-2.293-2.293a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </button>
         </div>
       </div>
