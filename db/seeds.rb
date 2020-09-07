8ccd3f01c33ced2eca893b4dbb31cbc3268d3699
abeidahmed <abeidahmed92@gmail.com>
2020-09-07 17:51:32 +0530

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = ['Cta sections', 'Navbars', 'Page headings', 'Sign-in and registrations', 'Buttons']
+
+categories.each do |category|
+  Category.create! title: category
+end
