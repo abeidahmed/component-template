f323a556e21f066d8fd01c2eee413c482e9cd640
abeidahmed <abeidahmed92@gmail.com>
2020-09-10 18:25:12 +0530

    add inter font cdn link in head tag

--- app/javascript/components/preview.vue
+++ app/javascript/components/preview.vue
@@ -149,6 +149,7 @@ export default {
           <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
             <link rel="stylesheet" href="${cssPath}"/>
           </head>
           <body>
