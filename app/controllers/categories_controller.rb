55899cfb5c549c6e8d27220630b08e24bb67bdaf
abeidahmed <abeidahmed92@gmail.com>
2020-09-10 19:00:40 +0530

    sort templates by name

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
-    @templates = templates_for(@category)
+    @templates ||= templates_for(@category).sort_by { |name| File.basename(name) }
   end
 end
