a56d001965ea271edb7ea8d670aa9904b0a1f181
abeidahmed <abeidahmed92@gmail.com>
2020-09-11 16:24:36 +0530

    create a dynamic category generator

--- lib/generators/category/category_generator.rb
+++ lib/generators/category/category_generator.rb
@@ -2,29 +2,55 @@ class CategoryGenerator < Rails::Generators::Base
   include ApplicationHelper
 
   source_root File.expand_path('templates', __dir__)
-  argument :folder_name, type: :string
+
+  argument :parent_name, type: :string
+  argument :title, type: :string
 
   def generate_category
-    if create_category
-      generate_category_folder
-    else
-      puts "Category #{folder_name} already exists"
-    end
+    create_category
   end
 
   private
-    def generate_category_folder
-      directory = "app/templates/#{underscorize(folder_name)}"
-      if File.exists?(directory)
-        puts "#{folder_name} already exists in app/templates"
+    def create_category
+      if parent_component_collection?
+        parent = ComponentCollection.find_by(title: parent_name.humanize)
+        create_category_and_folder_in(parent, 'components')
+      elsif parent_page_collection?
+        parent = PageCollection.find_by(title: parent_name.humanize)
+        create_category_and_folder_in(parent, 'pages')
       else
-        Dir.mkdir(directory)
-        puts "Created new folder #{folder_name}"
+        return puts 'Please type in the correct PARENT NAME'
       end
     end
 
-    def create_category
-      title = folder_name.humanize
-      Category.create!(title: title) unless Category.exists?(title: title)
+    def create_category_and_folder_in(parent, folder_name)
+      if Category.exists?(title: title.humanize)
+        return puts 'Category already exists!'
+      else
+        Category.create!(title: title.humanize, categorizable: parent)
+        if folder_name == 'components'
+          generate_category_in_components_folder
+        else
+          generate_category_in_pages_folder
+        end
+      end
+    end
+
+    def parent_component_collection?
+      ComponentCollection.exists?(title: parent_name.humanize)
+    end
+
+    def parent_page_collection?
+      PageCollection.exists?(title: parent_name.humanize)
+    end
+
+    def generate_category_in_components_folder
+      directory = Rails.root.join('app', 'components', underscorize(title))
+      Dir.mkdir(directory)
+    end
+
+    def generate_category_in_pages_folder
+      directory = Rails.root.join('app', 'pages', underscorize(title))
+      Dir.mkdir(directory)
     end
 end
