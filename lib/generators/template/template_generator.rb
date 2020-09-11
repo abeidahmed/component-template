467d2eb3f04aecc5f68eb84180106e72e3e28e25
abeidahmed <abeidahmed92@gmail.com>
2020-09-11 12:41:41 +0530

    comment out code that was used to test yaml_writer

--- lib/generators/template/template_generator.rb
+++ lib/generators/template/template_generator.rb
@@ -4,12 +4,24 @@ class TemplateGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
   argument :file_title, type: :string
 
+  # YAML_FILE_NAME = 'template_structure'
+
   def generate_category_title
     generate_template_boilerplate
+    # write_to_yaml
   end
 
   private
     def generate_template_boilerplate
       copy_file "template_boilerplate.html", "app/templates/#{underscorize(name)}/#{file_title}.html"
     end
+
+    # def write_to_yaml
+    #   YamlWriter.new(
+    #     type: 'components',
+    #     category: name,
+    #     template_name: file_title,
+    #     file_name: YAML_FILE_NAME
+    #   ).write!
+    # end
 end
