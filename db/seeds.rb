f374eb12ddd62ddd824d327e4c57d60132344941
abeidahmed <abeidahmed92@gmail.com>
2020-09-15 16:51:28 +0530

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-file_path = Rails.root.join('db', 'components.yml')
-doc = YAML::load_file(file_path)
+components = YAML::load_file(Rails.root.join('db', 'components.yml'))
 
-doc.each do |title, contents|
+components.each do |title, contents|
   component_collection = if ComponentCollection.exists? title: title.humanize
     ComponentCollection.find_by title: title.humanize
   else
@@ -24,6 +23,28 @@ doc.each do |title, contents|
     templates.each do |template|
       unless Component.exists? title: template.humanize
         Component.create! title: template.humanize, category_id: cat.id
+      end
+    end
+  end
+end
+
+pages = YAML::load_file(Rails.root.join('db', 'pages.yml'))
+
+pages.each do |title, contents|
+  component_collection = if PageCollection.exists? title: title.humanize
+    PageCollection.find_by title: title.humanize
+  else
+    PageCollection.create! title: title.humanize
+  end
+  contents.each do |category, templates|
+    cat = if Category.exists? title: category.humanize
+      Category.find_by title: category.humanize
+    else
+      Category.create! title: category.humanize, categorizable: component_collection
+    end
+    templates.each do |template|
+      unless Page.exists? title: template.humanize
+        Page.create! title: template.humanize, category_id: cat.id
       end
     end
   end
